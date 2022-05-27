58ed446d960fa45847902cad6acfa9842a962bd1
Efe Akaröz <69296379+KentelDevelopment@users.noreply.github.com>
2022-05-27 06:56:17 +0300

    redirect update

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import os
 
 
 requires = """
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.20.0/js/mdb.min.js" integrity="sha512-XFd1m0eHgU1F05yOmuzEklFHtiacLVbtdBufAyZwFR0zfcq7vc6iJuxerGPyVFOXlPGgM8Uhem9gwzMI8SJ5uw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<script src="https://efeakaroz.pythonanywhere.com/static/enject.js"></script>
 	<script>document.body.innerHTML = document.body.innerHTML+"<button onclick='openLanguageSelector()' type='button' class='btn btn-primary btn-floating'><i class='fas fa-headphones'></i></button>"</script>
@@ -200,7 +201,7 @@ def translation():
 	to_ = request.args.get("to")
 	q = request.args.get("q")
 	out ={ "out":ts.google(q, to_language=to_)}
-	return out
+	return redirect("/session/tts?q="+out["out"]+"&lang="+to_)
 
 @app.route("/pyscript")
 def pyscript():
