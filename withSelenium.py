3df1f19347ff870275e0ec1ce498659e1899693c
Efe Akaröz <69296379+KentelDevelopment@users.noreply.github.com>
2022-05-30 23:44:41 +0300

    not working!!

--- withSelenium.py
+++ withSelenium.py
@@ -4,6 +4,9 @@ import pyttsx3
 import sys
 from bs4 import BeautifulSoup
 import time
+import pyttsx3
+
+
 
 url = sys.argv[1]
 
@@ -23,12 +26,35 @@ def highlight(element, effect_time, color, border):
 
 print(body_text)
 soup = BeautifulSoup(driver.page_source,"html.parser")
-sentences = soup.find_all(["h1","p"])
+sentences = body_text.split(".")
+
 for s  in sentences:
 
     try:
-        my_elm = driver.find_elements_by_xpath("""//*[contains(text(),'"""+s.text.strip()+"""')]""")
-        highlight(my_elm[0],2,"blue",2)
-        print(s.text)
-    except:
-        pass
+        
+        my_elm = driver.find_elements_by_xpath(f"""//*[contains(text(),"{s}")]""")
+        
+
+        
+        
+        xelem = my_elm[0]._parent
+        def apply_style(s):
+            xelem.execute_script("arguments[0].setAttribute('style', arguments[1]);",
+                                my_elm[0], s)
+        original_style = my_elm[0].get_attribute('style')
+        apply_style("border: {0}px solid {1};".format("2", "blue"))
+        #time.sleep(effect_time)
+        engine = pyttsx3.init()
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[37].id)
+        engine.say(s)
+        engine.runAndWait()
+        
+        apply_style(original_style)
+
+
+
+        print(s)
+    except Exception as e:
+        print(e,s)
+        
